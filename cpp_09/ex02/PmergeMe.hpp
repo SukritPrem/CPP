4ff33642ffcc9b610e35c3384a726a3603df60c0
Sukrit Pipitkul <spipitku@z1t5c1.42bangkok.com>
2024-07-07 15:26:40 +0700

    sort maybe finish

--- cpp_09/ex02/PmergeMe.hpp
+++ cpp_09/ex02/PmergeMe.hpp
@@ -1,23 +1,166 @@
 #ifndef PMERGEME_HPP
 #define PMERGEME_HPP
-
+#include <algorithm>
 #include <vector>
 #include <iostream>
 #include <string>
-
-template<typename Container, typename Iterator>
+#include <stdio.h>
+template<typename Container, typename ContainPair>
 class PmergeMe
 {
     private:
-        Container _data;
+        Container _container;
+        ContainPair _containerPair;
+        bool _haveOdd;
+        int _aloneNum;
     public:
         PmergeMe(){
-
+            _haveOdd = false;
         };
-
-        void test(){
-            std::cout << test << std::endl;
+        void setData(Container &data){
+            _container = data;
         }
+
+        void insertContainerToContainerPair(){
+            // Iterator it;
+            std::pair<int, int> pairValue;
+            typename Container::const_iterator it;
+            // std::cout << _container.size() << std::endl;
+            if(_container.size() % 2 == 1)
+                _haveOdd = true;
+
+            for(it = _container.begin(); it < _container.end(); it++){
+                pairValue.first = *it;
+                if(it++ + 1 == _container.end())
+                {
+                    _aloneNum = pairValue.first;
+                    break;
+                }        
+                pairValue.second = *it;
+                _containerPair.push_back(pairValue);
+            }
+            // _containerPair.push_back(pairValue);
+        }
+
+        void printContainerPair(){
+            typename ContainPair::iterator it = _containerPair.begin();
+            while(it != _containerPair.end())
+            {
+                std::cout << it->first << std::endl;
+                std::cout << it->second << std::endl;
+                it++;
+            }
+        }
+
+        void printContainer(){
+            typename Container::iterator it = _container.begin();
+            while(it != _container.end())
+            {
+                std::cout << *it << std::endl;
+                it++;
+            }   
+        }
+
+        void sortContainerInsidePair(){
+            typename ContainPair::iterator it = _containerPair.begin();
+            std::pair<int, int> pairValue;
+            while(it != _containerPair.end())
+            {
+                if(it->first > it->second)
+                {
+                    int tmp = it->second;
+                    it->second = it->first;
+                    it->first = tmp;
+                }
+                it++;
+            }
+        }
+
+
+        void mergeSortContainerPair(){
+
+            mergeSort(_containerPair, 0, _containerPair.size() - 1);
+            
+        }
+
+        void merge(ContainPair &vec, int left, int mid, int right) {
+            int n1 = mid - left + 1;
+            int n2 = right - mid;
+
+            ContainPair leftVec(n1);
+            ContainPair rightVec(n2);
+
+            for (int i = 0; i < n1; ++i) {
+                leftVec[i] = vec[left + i];
+            }
+            for (int i = 0; i < n2; ++i) {
+                rightVec[i] = vec[mid + 1 + i];
+            }
+
+            int i = 0, j = 0, k = left;
+            while (i < n1 && j < n2) {
+                if (leftVec[i].first <= rightVec[j].first) {
+                    vec[k] = leftVec[i];
+                    ++i;
+                } else {
+                    vec[k] = rightVec[j];
+                    ++j;
+                }
+                ++k;
+            }
+
+            while (i < n1) {
+                vec[k] = leftVec[i];
+                ++i;
+                ++k;
+            }
+
+            while (j < n2) {
+                vec[k] = rightVec[j];
+                ++j;
+                ++k;
+            }
+        }
+
+        void mergeSort(ContainPair &container, int left, int right) {
+            if (left < right) {
+                int mid = left + (right - left) / 2;
+                mergeSort(container, left, mid);
+                mergeSort(container, mid + 1, right);
+                merge(container, left, mid, right);
+            }
+        }
+
+        void clearContainer(){
+            _container.clear();
+        }
+
+        void insertContainerPairToContainer(){
+            typename ContainPair::iterator it = _containerPair.begin();
+            while(it != _containerPair.end())
+            {
+                _container.push_back(it->first);
+                it++;
+            }    
+        }
+
+        void binarySearchContainer(){
+            typename ContainPair::iterator it = _containerPair.begin();
+            
+            while(it != _containerPair.end())
+            {
+                typename Container::iterator position = std::lower_bound(_container.begin(), _container.end(), it->second);
+                _container.insert(position, it->second);
+                it++;
+            }
+
+            if(_haveOdd)
+            {
+                typename Container::iterator position = std::lower_bound(_container.begin(), _container.end(), _aloneNum);
+                _container.insert(position, _aloneNum);
+            }
+        }
+
         ~PmergeMe(){
 
         };
--- cpp_09/ex02/main.cpp
+++ cpp_09/ex02/main.cpp
@@ -4,18 +4,48 @@
 #include "PmergeMe.hpp"
 #include <vector>
 #include <list>
+#include <utility>
+#include <string>
+
 int main(int argc,char **argv)
 {
-
+    (void)argc;
+    (void)argv;
+    std::string mocArgv[100] = {"9", "8", "2", "1", "3", "5", "4", "10", "12","6"};
     std::vector<int> vec;
     std::list<int> list;
-    for (int i = 1; i < argc; i++){
-        vec.push_back(std::atoi(argv[i]));
-        list.push_back(std::atoi(argv[i]));
+    // int mockArgc = 100;
+    int count = 0;
+    while (mocArgv[count] != "") {
+        count++;
     }
 
-    PmergeMe<std::vector<int>,std::vector<int>::iterator> test;
-    PmergeMe<std::list<int>,std::list<int>::iterator> test2;
+    // std::cout << count << std::endl;
+    for (int i = 0; i < count; i++){
+        // std::cout << mocArgv[i] << std::endl;
+        vec.push_back(std::atoi(mocArgv[i].c_str()));
+        list.push_back(std::atoi(mocArgv[i].c_str()));
+    }
 
+    PmergeMe<std::vector<int>, std::vector<std::pair<int,int> > > test;
 
+    // PmergeMe<std::list<int>,std::list<int>::iterator> test2;
+    test.setData(vec);
+    test.insertContainerToContainerPair();
+    test.sortContainerInsidePair();
+    // test.printContainerPair();
+    // std::cout << "-----------------------------" << std::endl;
+    test.mergeSortContainerPair();
+    // test.printContainerPair();
+    // std::cout << "-----------------------------" << std::endl;
+    test.clearContainer();
+    test.insertContainerPairToContainer();
+    test.printContainer();
+    std::cout << "-----------------------------" << std::endl;
+    test.binarySearchContainer();
+    std::cout << "-----------------------------" << std::endl;
+    test.printContainer();
+    // test2.setData(list);
+    // test.sortContainer();
+    // test.printOutput();
 }
